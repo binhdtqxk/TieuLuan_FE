5e22c5dd9c26241d53ef97e51f6c02798dbb0c22
binhdtqxk <146164453+binhdtqxk@users.noreply.github.com>
2025-03-21 23:12:42 +0700

    reset all for bad code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
 import './App.css'
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Login from './components/login/login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {routes} from './router'
 function App() {
 
   return (
-    <Router>
-      <Switch>
-          <Route path="/about">
-            about
-          </Route>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/">
-            abc
-          </Route>
-        </Switch>
-    </Router>
+    <BrowserRouter>
+     
+     <Routes>
+        {routes.map((item, index) => {
+          const Page = item.Component;
+          return <Route key={index} path={item.path} element={
+                <Page />
+          } />;
+        })}
+      </Routes>
+   
+      
+    </BrowserRouter>
   )
 }
 
---
+++ src/router.js
@@ -0,0 +1,9 @@
+import Authentication from "./components/Authentication/Authentication";
+import HomePage from "./components/HomePage/HomePage";
+
+export const routes=[
+    {
+        path: "/",
+        Component: true?HomePage:Authentication
+      },
+]
